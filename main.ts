5f3cf38daa828660ba8627ef11859bfeecf95e72
Yuruie <122543839+Yuruie@users.noreply.github.com>
2023-01-31 13:11:47 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,16 +8,32 @@ function Rps () {
     OLED.newLine()
     OLED.writeStringNewLine("Ties" + Ties)
 }
+input.onButtonPressed(Button.A, function () {
+    OLED.writeStringNewLine("a")
+    OLED.clear()
+    Player1 += 1
+    Rounds += 1
+    updatescores()
+})
+function updatescores () {
+	
+}
+input.onGesture(Gesture.Shake, function () {
+    reset()
+})
+function reset () {
+    OLED.init(128, 64)
+    OLED.writeStringNewLine("Shall We Play A Game")
+    Rounds = 0
+    Player1 = 0
+    Player2 = 0
+    Ties = 0
+    basic.pause(2000)
+    OLED.clear()
+    Rps()
+}
 let Ties = 0
+let Rounds = 0
 let Player2 = 0
 let Player1 = 0
-let Rounds = 0
-OLED.init(128, 64)
-OLED.writeStringNewLine("Shall We Play A Game")
-Rounds = 0
-Player1 = 0
-Player2 = 0
-Ties = 0
-basic.pause(2000)
-OLED.clear()
-Rps()
+reset()
